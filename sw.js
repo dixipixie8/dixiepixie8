ef322dde4045ad2819ab8a9cbf9fb7aadc68a1b0
dixipixie8 <dcidhidalgo@gmail.com>
2026-05-13 19:40:17 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,25 @@
-const CACHE = 'dixipixie8-v2';
+const CACHE = 'dixipixie8-v3';
 
-self.addEventListener('install', e => {
+self.addEventListener('install', function(e) {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+  }));
   self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', function(e) {
+  // NO interceptar peticiones a Google Scripts ni externas
+  var url = e.request.url;
+  if (url.indexOf('script.google.com') > -1 || url.indexOf('googleapis.com') > -1) {
+    return; // dejar pasar sin interceptar
+  }
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request);
+    })
   );
 });
